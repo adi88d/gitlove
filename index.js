38ed2a14e519a82f471ad4f623a597f3362cf2a1
Ephi Gabay <gabay.ephi@gmail.com>
2019-04-04 20:48:26 +0300

    heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+    // .use(express.static(path.join(__dirname, 'public')))
+    // .set('views', path.join(__dirname, 'views'))
+    // .set('view engine', 'ejs')
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', (req, res) => res.send('Hello World!'));
