0c3dbc9aee0c8fd4efc88798e830c7b3c03b9355
Ephi Gabay <gabay.ephi@gmail.com>
2019-04-04 20:56:48 +0300

    nice index

---
+++ express.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+// .use(express.static(path.join(__dirname, 'public')))
+// .set('views', path.join(__dirname, 'views'))
+// .set('view engine', 'ejs'
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+module.exports = {
+    app
+};
--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-const app = express();
-    // .use(express.static(path.join(__dirname, 'public')))
-    // .set('views', path.join(__dirname, 'views'))
-    // .set('view engine', 'ejs'
+const {app} = require("./express.js");
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
