14409bc69206bc6de96ae9ebd6a102ddb454965c
Ephi Gabay <gabay.ephi@gmail.com>
2019-04-04 23:48:13 +0300

    cross origin

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { getLanguages } = require("./github-helper.js");
 const { Users } = require("./collections/users");
 const { getRestaurants } = require("./tenbis/index");
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/filllanguage/:githubUsername/:userId", (req, res) => {
     return getLanguages(req.params.githubUsername)
