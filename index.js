227e1a85fe2c5f9423957218d63c71036b626baa
Ephi Gabay <gabay.ephi@gmail.com>
2019-04-04 20:50:54 +0300

    expressjs

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const app = express()
+const app = express();
     // .use(express.static(path.join(__dirname, 'public')))
     // .set('views', path.join(__dirname, 'views'))
-    // .set('view engine', 'ejs')
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    // .set('view engine', 'ejs'
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
